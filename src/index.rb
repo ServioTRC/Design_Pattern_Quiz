84e4e04976cc0c23ee0d5be92d90f0891b5ce1c4
ServioTRC <A01371719@itesm.mx>
2019-11-20 03:34:14 +0000

    Home web page working

--- src/index.rb
+++ src/index.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
+require './models/microservices'
+
+MICROSERVICES = Microservices.new
 
 get '/' do
   erb :home
 end
 
 post '/quiz' do
-    params['question_number']
+  MICROSERVICES.get_questions(params['question_number'])
+  # POP QUESTIONS UNTIL EMPTY
+  # SHOW FINAL SCORE, SAVE, ALL SCORES
+end
+
+post '/score' do
+    "pagina de scores"
 end
--- src/models/microservices.rb
+++ src/models/microservices.rb
@@ -1,20 +1,20 @@
-require 'Faraday'
+require 'faraday'
 
 QUESTIONS_URL = 'https://a00jkeefm6.execute-api.us-west-2.amazonaws.com/default/questions_obtaintion'
 
 class Microservices
   
-  include Singleton
+  #include Singleton
   
   def initialize
-    @questions_conn = Faraday.new :url => QUESTIONS_ENDPOINT
+    @questions_conn = Faraday.new(url: QUESTIONS_URL, headers: {'Content-Type': 'application/json'})
     @questions_array = []
   end
   
   def post_to_url(conn, key, body)
     res = conn.post do | req |
         # req.headers['x-api-key'] = key
-        req.body = body
+        req.body = body.to_json
     end
     JSON.parse(res.body)
   end
@@ -22,7 +22,7 @@ class Microservices
   def get_from_url(conn, key, body)
     res = conn.get do | req |
         # req.headers['x-api-key'] = key
-        req.body = body
+        req.body = body.to_json
     end
     JSON.parse(res.body)
   end
